b96e14c82cf5d416edde09882e00aa542e3cc5a7
fadhelmurphy <krazefollow@gmail.com>
2022-11-20 18:30:39 +0000

    Deploying to gh-pages from  @ cde25ae27677534e26926d30d047b6c50ad2b54d 🚀

---
+++ _next/static/wwWmTH3Q-FTOKpw4mQ_CD/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/193-c0859a07e63e97b3.js","static/chunks/pages/index-5ed248e202e76cec.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
