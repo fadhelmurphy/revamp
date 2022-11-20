dfa75620d505e7302db0cc96a86b9c58d9d9c29c
Padelpop Eskrim <fadhelijlalfalah@gmail.com>
2022-11-21 01:19:17 +0700

    fix: images

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  images: {
+    domains: ["via.placeholder.com"],
+    loader: "akamai",
+    path: "",
+  },
 };
 
 module.exports = nextConfig;
