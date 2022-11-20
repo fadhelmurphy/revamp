ff5c74bd7b6c928af6541ad815e87866fc788ef4
Padelpop Eskrim <fadhelijlalfalah@gmail.com>
2022-11-20 11:57:04 +0700

    feat: setup ci/cd

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    basePath: "/revamp",
+    assetPrefix: "/revamp",
+  };
+  
+  module.exports = nextConfig;
