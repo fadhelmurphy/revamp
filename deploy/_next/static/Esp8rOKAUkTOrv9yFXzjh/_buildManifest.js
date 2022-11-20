7a215fe00a66dd9074c0e637992d021c0d5c9fbb
Padelpop Eskrim <fadhelijlalfalah@gmail.com>
2022-11-20 11:47:17 +0700

    feat: add deploy folder

---
+++ deploy/_next/static/Esp8rOKAUkTOrv9yFXzjh/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/738-69df94aecde32951.js","static/chunks/pages/index-b7c40eaf5b0e20e1.js"],"/_error":["static/chunks/pages/_error-8e848193de3c139c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
