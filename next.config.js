6d836ed0d807fb35a70bae793918179cbec3de4a
Padelpop Eskrim <fadhelijlalfalah@gmail.com>
2022-11-21 00:47:51 +0700

    feat: update styling

--- next.config.js
+++ next.config.js
@@ -3,13 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
-  images: {
-    loader: "akamai",
-    path: "",
-  },
-  images: {
-    domains: ["via.placeholder.com"],
-  },
 };
 
 module.exports = nextConfig;
