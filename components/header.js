5b37db7a540acec60123571597c877ca0819d67b
Padelpop Eskrim <fadhelijlalfalah@gmail.com>
2023-01-01 21:17:24 +0700

    fix: background

--- components/header.js
+++ components/header.js
@@ -4,7 +4,7 @@ import LogoSlider from "./logo-slider";
 const Header = ({yourname, yourRole}) => (
   <>
     <div className="h-52 md:h-[55vh] mb-6 md:mb-24 px-8 md:px-0 flex flex-col justify-end">
-      <div className="inner container mx-auto text-left md:text-center">
+      <div className="inner container md:mx-auto text-left md:text-center">
       <p className="text-2xl md:text-6xl text-base-2 font-semibold font-header">
         {yourname}
       </p>
@@ -25,7 +25,7 @@ Header.propTypes = {
 Header.defaultProps = {
   yourname: "Hi, I'm Fadhel. 👋",
   yourRole: <>
-  <span className="relative stabilo">Software</span> Engineer <br/>Based in Tangerang
+  <span className="relative stabilo after:h-[1vh] md:h-[4vh]">Software</span> Engineer <br/>Based in Tangerang
   <style jsx>
         {`
         .stabilo {
@@ -34,7 +34,6 @@ Header.defaultProps = {
         .stabilo::after {
             content: "";
             width: 100%;
-            height: 4vh;
             transform: rotate(-2deg) skew(5deg);
             background-color: rgb(254 202 202);
             background-image: linear-gradient(to right,#f9d2af, #fdf2f8);
