e79705500d651ef0a495df7f4e4e89f90e949be6
fadhelmurphy <krazefollow@gmail.com>
2023-01-01 14:04:38 +0000

    Deploying to gh-pages from  @ 65c96e90c64b04e38e8bb091c0e6b092aa87bdae 🚀

---
+++ _next/static/6fBbcW_f3VctN3LnKi457/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/193-8cfe639b31ab086f.js","static/chunks/pages/index-3706b92085268be4.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
