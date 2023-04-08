1c3942b8ed901fd814884fe9eb8a7a1b3188f76a
fadhelmurphy <krazefollow@gmail.com>
2023-04-08 08:18:06 +0000

    Deploying to gh-pages from  @ 17ca369a350a94288aa42b507da37c2192fc0372 🚀

---
+++ _astro/timeline.122b4bac.js
@@ -0,0 +1,28 @@
+import{R as n}from"./index.f1bc5ebf.js";import{j as e}from"./jsx-runtime.109e40f8.js";const a=[{company:"Female Daily Network.",data:[{jobtitle:"Frontend Developer - Full-time",time:"Oct 2022 - Present",description:`
+          Develop Payment for cross-platform (iOS and Android) with Micro-frontend method <br />
+          Research latest tech-stack <br />
+          Deployment with Pm2 + AWS Server and Docker + Kubernetes <br />
+          Develop and Maintain web application <br />
+          Trace error with Sentry and New Relic <br />
+          Create guidelines for other frontend engineers <br />
+          `},{jobtitle:"Jr. Frontend Developer - Full-time",time:"Feb 2022 - Oct 2022",description:`
+          Improve homepage and brand list page performance score from 60 to
+          80-90 <br />
+          Implement Incremental Static Regeneration (ISR) on Homepage and
+          Product detail page <br />
+          Implement Server Side Rendering (SSR) and Client Side Rendering (CSR)
+          on several pages <br />
+          Create payment widget for cross platform with Webpack 5 <br />
+          Create RSS Feed for data analytics in Next.js <br />
+          Create reusable components with Monorepo using Lerna <br />
+          Integrate API and Create API Proxy using Fetch API, API routes
+          Next.js, and Redux-saga <br />
+          `}],isLatest:!0},{company:"ISTIH & SPIN | Kemenperin.",data:[{jobtitle:"Frontend Developer - Freelance",time:"Dec 2022 - Present",description:`
+          Setup and develop Map with realtime data using Next.js <br/>
+  Slicing UI from Figma to Web using Ant Design <br/>
+  Integrate API using Fetch API and state management with React Context <br/>
+          `},{jobtitle:"Frontend Developer - Freelance",time:"Dec 2020 - Feb 2021",description:`
+          Develop Marketplace using Next.js <br/>
+  Slicing UI from Figma to Web using Ant Design <br/>
+  Integrate API using Fetch API and state management with Redux-saga <br/>
+          `}],isLatest:!0}],i=()=>e.jsx("ol",{className:"relative my-12 mx-5 border-l border-blue-400",children:a.map(t=>e.jsxs("li",{className:"mb-10 ml-10",children:[e.jsx("span",{className:"absolute -left-4 mt-3 flex h-8 w-8 items-center justify-center rounded-full bg-blue-400 ring-8 ring-white ring-blue-100",children:e.jsx("svg",{"aria-hidden":"true",className:"h-5 w-5 text-white",fill:"currentColor",viewBox:"0 0 20 20",xmlns:"http://www.w3.org/2000/svg",children:e.jsx("path",{"fill-rule":"evenodd",d:"M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z","clip-rule":"evenodd"})})}),e.jsxs("h3",{className:"mb-1 mb-6 flex items-center text-6xl font-semibold",children:[t.company,t.isLatest?e.jsx("span",{className:"mr-2 ml-3 rounded bg-sky-100 px-3.5 py-1.5 text-sm font-medium text-blue-500",children:"Latest"}):null]}),t.data.map(r=>e.jsxs(e.Fragment,{children:[e.jsx("time",{className:"mb-2 block font-normal leading-6",children:r.jobtitle}),e.jsx("time",{className:"mb-2 block font-normal leading-6 text-base-2",children:r.time}),e.jsx("p",{className:"font-normal text-base-2 py-5",dangerouslySetInnerHTML:{__html:r.description}}),e.jsx("br",{})]}))]}))}),o=n.memo(i);export{o as default};
