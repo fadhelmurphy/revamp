e0b2da3fd4bfe84c8b755675ec603c72c8084feb
fadhelmurphy <krazefollow@gmail.com>
2023-04-09 08:09:40 +0000

    Deploying to gh-pages from  @ ee070ec8b30496855b4b64a2f11075c8bda415b3 🚀

---
+++ _astro/timeline.a7ebaacd.js
@@ -0,0 +1,40 @@
+const e=[{company:"Female Daily Network.",data:[{jobtitle:"Frontend Developer - Full-time",time:"Oct 2022 - Present",description:`
+          Develop Payment for cross-platform (iOS and Android) with Micro-frontend method (Webpack 5) <br />
+          Research latest tech-stack <br />
+          Deployment with Pm2 + AWS Server and Docker + Kubernetes <br />
+          Develop and Maintain web application <br />
+          Trace error with Sentry and New Relic <br />
+          Create guidelines for other frontend engineers <br />
+          `},{jobtitle:"Jr. Frontend Developer - Full-time",time:"Feb 2022 - Oct 2022",description:`
+          Improve homepage and brand list page performance score from 60 to
+          80-90 <br />
+          Implement Incremental Static Regeneration (ISR) on Homepage and
+          Product detail page <br />
+          Implement Server Side Rendering (SSR) and Client Side Rendering (CSR)
+          on several pages <br />
+          Create payment widget for cross platform with Webpack 5 <br />
+          Create RSS Feed for data analytics in Next.js <br />
+          Create reusable components with Monorepo using Lerna <br />
+          Integrate API and Create API Proxy using Fetch API, API routes
+          Next.js, and Redux-saga <br />
+          `}],isLatest:!0},{company:"ISTIH & SPIN | Kemenperin.",data:[{jobtitle:"Frontend Developer - Freelance",time:"Dec 2022 - Present",description:`
+          Setup and develop Map with realtime data using Next.js <br/>
+  Slicing UI from Figma to Web using Ant Design <br/>
+  Integrate API using Fetch API and state management with React Context <br/>
+          `},{jobtitle:"Frontend Developer - Freelance",time:"Dec 2020 - Feb 2021",description:`
+          Develop Marketplace using Next.js <br/>
+  Slicing UI from Figma to Web using Ant Design <br/>
+  Integrate API using Fetch API and state management with Redux-saga <br/>
+          `}],isLatest:!0},{company:"ENO | Venture Mind Labs.",data:[{jobtitle:"Frontend Developer - Freelance",time:"Mar 2022 - May 2022",description:`
+        Web Development with React, SWR, Chakra UI and Zustand
+        `}],isLatest:!1},{company:"Incubator Business Center Gunadarma.",data:[{jobtitle:"Frontend Developer - Internship",time:"Sep 2021 - Jan 2022",description:`
+        Web Development with Next.js (React.js) + React Context  <br/>
+Implementing UI Design from Figma to Bootstrap, HighChart, Antd (Slicing)  <br/>
+<br/>
+Project: <br/>
+SIASN Instansi BKN  <br/>
+Dashboard Keluarga Sehat Kemenkes  <br/>
+        `}],isLatest:!1},{company:"BisaCPNS.",data:[{jobtitle:"Frontend Developer - Freelance",time:"May 2020",description:`
+            Web development with native stack (HTML & CSS) + Bootstrap  <br/>
+            Develop custom on scroll animation with AOS.js <br/>
+            `}],isLatest:!1}];export{e as t};
